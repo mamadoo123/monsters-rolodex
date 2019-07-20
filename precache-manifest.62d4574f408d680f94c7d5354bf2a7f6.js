91f2d15bc50331a59aca831219f47bb7a4d4a3db
mohamed khamis <mklhs66662000@gmail.com>
2019-07-20 02:33:53 +0200

    Updates

---
+++ precache-manifest.62d4574f408d680f94c7d5354bf2a7f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c3f3f5c3.js"
+  },
+  {
+    "revision": "9ab18eb0ccc2d11e69c0",
+    "url": "/monsters-rolodex/static/js/main.9ab18eb0.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.681b5105.chunk.js"
+  },
+  {
+    "revision": "9ab18eb0ccc2d11e69c0",
+    "url": "/monsters-rolodex/static/css/main.e4512d55.chunk.css"
+  },
+  {
+    "revision": "b3add821a3634ca13f0528e5e6aaf4b2",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
