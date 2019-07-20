fa59ed31898cc7a2374d83d44914668dbefcec51
mohamed khamis <mklhs66662000@gmail.com>
2019-07-20 02:37:17 +0200

    adding files to github pages

--- src/index.js
+++ src/index.js
@@ -9,4 +9,12 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/* =============================
+    mohamed khamis instructions:
+=================================
+    i have solved create-react-app issue by uninstalling
+    react-scripts version 3.0.1 devDependency and then 
+    reinstall it using an older version 2.2.5
+    using yarn commands           
+    </p> */
+serviceWorker.register();
